6f493105814097ab890ecb2c92a76a29def35012
64010051 <88467913+Kitti-Chumchuen@users.noreply.github.com>
2023-04-21 13:58:18 +0700

    Delete WEB-APP-PROJECT directory

--- WEB-APP-PROJECT/WEB-APP-PROJECT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WEB_APP_PROJECT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
